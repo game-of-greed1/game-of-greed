06b4404cc8825001b113112fe86640bab2b9c8b4
dina-fouad <dina_albarghouthi@yahoo.com>
2021-08-03 14:38:35 +0300

    version3

--- game_of_greed/game.py
+++ game_of_greed/game.py
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-from game_of_greed.game_logic import GameLogic
-from game_of_greed.banker import Banker
-
-class Game:
-    def __init__(self):
-        self.banker=Banker()
-    
-    def play(self,roller=None):
-        self.roller=roller or GameLogic.roll_dice
-        print("Welcome to Game of Greed")
-        user_input = input("(y)es to play or (n)o to decline")
-        if user_input =="y" or user_input=="yes":
-          self.game()
-
-        else :
-         print("OK. Maybe another time")
-      
-
- 
-=======
 from game_of_greed import game_logic
 from game_of_greed.game_logic import GameLogic
 from game_of_greed.banker import Banker
@@ -86,4 +65,3 @@ class Game(GameLogic, Banker):
 if __name__ == "__main__":
     game = Game()
     game.play()
->>>>>>> a3af087f8bfebccacf86de2defc780af8f860925
