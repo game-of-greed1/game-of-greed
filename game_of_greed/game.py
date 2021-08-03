2aa9176f5af728ee3f43c6598cb0ecbd811b4443
dina-fouad <dina_albarghouthi@yahoo.com>
2021-08-03 14:15:18 +0300

    lab7

--- game_of_greed/game.py
+++ game_of_greed/game.py
@@ -1,19 +1,67 @@
+from game_of_greed import game_logic
 from game_of_greed.game_logic import GameLogic
 from game_of_greed.banker import Banker
 
-class Game:
-    def __init__(self):
-        self.banker=Banker()
-    
-    def play(self,roller=None):
-        self.roller=roller or GameLogic.roll_dice
+
+class Game(GameLogic, Banker): 
+
+    def play(self, roller=None):
+        roller=roller or Game.roll_dice
         print("Welcome to Game of Greed")
-        user_input = input("(y)es to play or (n)o to decline")
-        if user_input =="y" or user_input=="yes":
-          self.game()
+        print("(y)es to play or (n)o to decline")
+        ask_User = input("> ")
 
-        else :
-         print("OK. Maybe another time")
-      
+        if ask_User == "y":
+            in_play = True
+            rounds_number = 1
+            dice_number = 6
+            while in_play:
+                print(f"Starting round {rounds_number}")
+                print(f"Rolling {dice_number} dice...")
+                dice = roller(dice_number)
+                Game.print_random_dice(dice)
+                print("Enter dice to keep, or (q)uit:")
+                keep_or_leave = input("> ") 
+                if keep_or_leave == "q":
+                    in_play = False
+                    print(f"Thanks for playing. You earned {self.balance} points")
+                else:
+                    dice_value = []
+                    for x in keep_or_leave:
+                        dice_value.append(int(x))
+                    tuple_list = tuple(dice_value)
+                    temp_shelf = self.calculate_score(tuple_list)
+                    self.shelf(temp_shelf)
+                    dice_number = dice_number - len(keep_or_leave) 
+                    print(f"You have {self.shelved} unbanked points and {dice_number} dice remaining")
+                    print("(r)oll again, (b)ank your points or (q)uit:")
+                    ask_User2 = input("> ")
+                    if ask_User2 == "r":
+                        continue
+                    elif ask_User2 == "b":
+                        print(f"You banked {self.shelved} points in round {rounds_number}")
+                        self.bank()
+                        rounds_number += 1
+                        print(f"Total score is {self.balance} points")
+                        dice_number = 6
+                    elif ask_User2 == "q":
+                        print(f"Thanks for playing. You earned {self.balance} points")
+                        in_play = False
+        else: 
+            print("OK. Maybe another time")
+    
 
- 
+    @staticmethod
+    def print_random_dice(dice):
+        first_string = "*** "
+        last_string = "***"
+        for x in dice:
+            first_string = first_string + str(x) + " " 
+        first_string+=last_string
+        print(first_string)
+    
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.play()
